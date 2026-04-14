02d2e1051b389f2811746285cc3f7ee171822f4a
AbdelrahmanSamirz <shekaba@gmail.com>
2026-04-14 19:32:09 +0200

    Question01

---
+++ DataSources/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment_LINQ_Session02_
+{
+	public class Student
+	{
+		public string Name { get; set; }
+		public int Grade { get; set; }
+		public string Major { get; set; }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Assignment_LINQ_Session02_
+﻿    using static Assignment_LINQ_Session02_.Source;
+namespace Assignment_LINQ_Session02_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            ////1. Get top 3 most expensive products
+            //var result = ProductList.OrderByDescending(p => p.UnitPrice)
+            //                .Take(3);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
